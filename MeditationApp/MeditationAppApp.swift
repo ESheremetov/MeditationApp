a39cbe9b2750d24d98ca7d1705d5e9246b7ea6dc
egor_sheremetov <georgesheremetov3.0@gmail.com>
2021-11-29 19:00:46 +0300

    Initial Commit

---
+++ MeditationApp/MeditationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationAppApp.swift
+//  MeditationApp
+//
+//  Created by Егор Шереметов on 29.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct MeditationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
